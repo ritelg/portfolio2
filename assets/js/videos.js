8a40f7bdd7f7de5c4dbabd3ca10d3cadc330f78f
ritelg <ritelg@yahoo.fr>
2021-03-26 09:13:28 +0100

    Refactoring 3

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,5 +1,11 @@
 /**
  * @param {string} src Lien de la vidéo
+ * @param {Object} optionsVideos
+ * @param {boolean} optionsVideos.muted
+ * @param {boolean} optionsVideos.autoplay
+ * @param {boolean} optionsVideos.loop
+ * @param {boolean} optionsVideos.mp4
+ * @param {boolean} optionsVideos.ogg
  */
 export function createVideo(src, classes, optionsVideos = {}) {
     optionsVideos = Object.assign({
@@ -11,19 +17,19 @@ export function createVideo(src, classes, optionsVideos = {}) {
     }, optionsVideos)
 
     const video = document.createElement('video')
-    video.autoplay = optionsVideos['autoplay']
-    video.loop = optionsVideos['loop']
-    video.muted = optionsVideos['muted']
+    video.autoplay = optionsVideos.autoplay
+    video.loop = optionsVideos.loop
+    video.muted = optionsVideos.muted
     video.classList = classes
     
-    if (optionsVideos['mp4']) {
+    if (optionsVideos.mp4) {
         const sourceMp4 = document.createElement('source')
         sourceMp4.setAttribute('src', src + '.mp4')
         sourceMp4.setAttribute('type','video/mp4')
         video.appendChild(sourceMp4)
     }
 
-    if (optionsVideos['ogg']) {
+    if (optionsVideos.ogg) {
         const sourceOgg = document.createElement('source')
         sourceOgg.setAttribute('src', src + '.ogg')
         sourceOgg.setAttribute('type', 'video/ogg')
